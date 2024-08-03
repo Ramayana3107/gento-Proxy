2307fb7bfd9458c31b0625e32491fc5e07c3253d
Ramayana3107 <ramayana3107@gmail.com>
2024-08-03 23:14:41 +0700

    proxy

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x70766b1Bf6E71a7BFeD11B60C769220697aDc6Ce'
+export const ProxyAdmin = '0xc0b0129DEFfEa7416F4003fC83249DB8eB695a05'
+export const SWTRImplementation = '0x8bAF90065253658D70C0444bc6f4641290211bd8'
+    
